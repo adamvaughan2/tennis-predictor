f6f805cf76f9a060f04a778a56ea0cebd8d9d239
adamvaughan <151431693+adamvaughan2@users.noreply.github.com>
2025-05-12 09:59:27 +0100

    Change app layout to 2 columns

--- app.py
+++ app.py
@@ -30,46 +30,47 @@ Created by Adam Vaughan.
 """)
 
 # main content
+col1, col2 = st.columns([1,1])
 
-# create a form for user to input Player 1, Player 2 and Surface
-with st.form(key="prediction_form"):
-    st.subheader("Match Prediction")
-    
-    # input dropdowns for user to select player names and surface
-    # @TODO: restrict to selection to active players (match in last year?)
-    player_A = st.selectbox("Select Player 1", predictor.players, index=predictor.players.index("Sinner J."))
-    player_B = st.selectbox("Select Player 2", predictor.players, index=predictor.players.index("Alcaraz C."))
-    surface = st.selectbox("Select Surface", predictor.surfaces)
-    
-    # submit button
-    submit_button = st.form_submit_button(label='Predict Match Outcome')
-
-# if the button is clicked, make prediction
-if submit_button:
-    if player_A == player_B:
-        st.error("Please select two different players.")
-    else:
-        # make prediction
-        winner, probability = predictor.predict_winner(player_A, player_B, surface)
+with col1:
+    # create a form for user to input Player 1, Player 2 and Surface
+    with st.form(key="prediction_form"):
+        st.subheader("Match Prediction")
         
-        # display the result
-        st.success(f"The predicted winner is: **{winner}** with a probability of **{round(100*probability)}%**")
+        # input dropdowns for user to select player names and surface
+        player_A = st.selectbox("Select Player 1", predictor.players, index=predictor.players.index("Sinner J."))
+        player_B = st.selectbox("Select Player 2", predictor.players, index=predictor.players.index("Alcaraz C."))
+        surface = st.selectbox("Select Surface", predictor.surfaces)
+        
+        # submit button
+        submit_button = st.form_submit_button(label='Predict Match Outcome')
 
-    # @TODO: add feature data to the output
-    st.write("Feature data of this match:")
-    # create a table
-    feature_df = predictor.get_feature_data(player_A, player_B, surface)
+with col2:
+    # if the button is clicked, make prediction
+    if submit_button:
+        if player_A == player_B:
+            st.error("Please select two different players.")
+        else:
+            # make prediction
+            winner, probability = predictor.predict_winner(player_A, player_B, surface)
+            
+            # display the result
+            st.success(f"The predicted winner is: **{winner}** with a probability of **{round(100*probability)}%**")
 
-    display_feature_df = pd.DataFrame(
-        columns=[player_A, "Player", player_B],
-        data=[
-            [int(feature_df["Rank A"][0]), "Rank", int(feature_df["Rank B"][0])],
-            [round(100*feature_df["Form A"][0]), "Form (win % in last 90 days)", round(100*feature_df["Form B"][0])],
-            [round(100*feature_df["H2HWinProportion A"][0]), "H2H Win %", round(100*feature_df["H2HWinProportion B"][0])],
-            [round(100*feature_df["SurfaceWinProportion A"][0]), "Surface Win %", round(100*feature_df["SurfaceWinProportion B"][0])],
-        ],
-    )
+        st.write("Feature data of this match:")
+        # create a table
+        feature_df = predictor.get_feature_data(player_A, player_B, surface)
 
-    st.dataframe(display_feature_df, hide_index=True)
+        display_feature_df = pd.DataFrame(
+            columns=[player_A, "Player", player_B],
+            data=[
+                [int(feature_df["Rank A"][0]), "Rank", int(feature_df["Rank B"][0])],
+                [round(100*feature_df["Form A"][0]), "Form (win % in last 90 days)", round(100*feature_df["Form B"][0])],
+                [round(100*feature_df["H2HWinProportion A"][0]), "H2H Win %", round(100*feature_df["H2HWinProportion B"][0])],
+                [round(100*feature_df["SurfaceWinProportion A"][0]), "Surface Win %", round(100*feature_df["SurfaceWinProportion B"][0])],
+            ],
+        )
+
+        st.dataframe(display_feature_df, hide_index=True)
 
     # @TODO: add previous H2H matches to the output
